34eaaae1495317e8561f2a4d56805e2459bd6b31
Kleo Bercero <kbeflo@gmail.com>
2019-05-23 11:34:20 +0800

    Update logo

--- assets/js/pilantropo.js
+++ assets/js/pilantropo.js
@@ -1,17 +1,16 @@
 anime({
-  targets: "#one .lines path",
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: "easeInOutSine",
-  duration: 1000,
-  delay: function(e, a) {
-    return 250 * a
-  },
-  direction: "normal"
-}), anime({
+  targets: '#one .lines path',
+  translateX: [-23, 0], // from 100 to 250
+  delay: 200,
+  direction: 'normal',
+  loop: false
+});
+
+anime({
   targets: "#two .lines path",
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: "easeInOutSine",
-  duration: 1000,
+  duration: 2000,
   delay: function(e, a) {
     return 250 * a
   },
@@ -21,7 +20,7 @@ anime({
   targets: "#three .lines path",
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: "easeInOutSine",
-  duration: 1000,
+  duration: 2000,
   delay: function(e, a) {
     return 250 * a
   },
@@ -30,22 +29,34 @@ anime({
 }),
 
 anime({
-  targets: "#one .lines path",
-  fill: ["#fe0067", "#fff"],
-  direction: "reverse",
-  duration: 1000
-})
-
-anime({
   targets: "#two .lines path",
   fill: ["#fe0067", "#fff"],
   direction: "reverse",
-  duration: 1500
+  duration: 2500
 })
 
 anime({
   targets: "#three .lines path",
   fill: ["#fe0067", "#fff"],
   direction: "reverse",
-  duration: 2000
+  duration: 3000
+})
+
+anime({
+  targets: "#ilantropo .lines path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "easeInOutSine",
+  duration: 2000,
+  delay: function(e, a) {
+    return 250 * a
+  },
+  direction: "normal",
+  delay: "1500"
+}),
+
+anime({
+  targets: "#ilantropo .lines path",
+  fill: ["#fe0067", "#fff"],
+  direction: "reverse",
+  duration: 3000
 })
